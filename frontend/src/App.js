203f48dfbceadc06c3ad13350cc345ba891cdc55
marvin <marvin131t@gmail.com>
2022-04-05 08:35:36 +0200

    Added react support

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Drink from './components/Drink';
+
+function App() {
+  return (
+      <React.Fragment>
+        <div>
+          <Drink />
+        </div>
+      </React.Fragment>
+  );
+}
+
+export default App;
